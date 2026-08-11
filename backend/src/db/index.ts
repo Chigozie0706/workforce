0eb80636f972f326302113c005477075ee042b3c
chigozie0706 <chigoziejacob@gmail.com>
2026-08-11 02:28:13 +0100

    added build

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,6 +2,11 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("localhost")
+    ? false
+    : { rejectUnauthorized: false },
+});
 
 export const db = drizzle(pool, { schema });
